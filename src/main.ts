c8122d63121a8c9d221a4e22c3649247dad95321
Vero <facu2000veronelli@outlook.com>
2022-01-06 16:31:51 -0300

    add num port env

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use('/public', express.static(join(__dirname, '..', 'file', 'cv')));
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
